b80c72472b42290f921266e98a6a515d12944c6e
rjshrjndrn <rjshrjndrn@gmail.com>
2025-05-21 14:22:36 +0200

    chore(plugins): open original file directly rather than symlink
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -14,4 +14,14 @@ return {
     "rjshrjndrn/friendly-snippets",
     branch = "patch-1",
   },
+  {
+    "aymericbeaumet/vim-symlink",
+    dependencies = { "moll/vim-bbye" }, -- optional, for better buffer management
+    event = "BufReadPre", -- Load just before reading a buffer
+    cond = function() -- load only if the current file is a symlink
+      -- Check if current file is a symlink
+      local stats = vim.uv.fs_lstat(vim.fn.expand("%:p"))
+      return stats and stats.type == "link"
+    end,
+  },
 }
