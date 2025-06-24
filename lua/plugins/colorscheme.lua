5009acdd53096a9817940faabd795b109568ed8d
rjshrjndrn <rjshrjndrn@gmail.com>
2025-06-24 14:00:26 +0200

    update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,7 +18,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "terafox",
+      colorscheme = "duskfox",
     },
   },
 }
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 -- { "folke/flash.nvim", enabled = false },
 -- { "folke/todo-comments.nvim", enabled = true, commit = "ae0a2afb47cf7395dc400e5dc4e05274bf4fb9e0" },
 return {
-  { commit = "7c36f6d459bd8a15c77fbfa75fbc843b2c18af87", "echasnovski/mini.ai" },
+  -- { commit = "7c36f6d459bd8a15c77fbfa75fbc843b2c18af87", "echasnovski/mini.ai" },
 }
