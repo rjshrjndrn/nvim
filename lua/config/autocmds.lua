81261a45f1f3d624c0afe07a9847f12fa2aea579
rjshrjndrn <rjshrjndrn@gmail.com>
2025-07-07 12:05:05 +0200

    feat: lsp auto ft for argo

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,3 +13,28 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.opt_local.writebackup = false -- No backup while writing
   end,
 })
+-- Create automatic file type for argocd, so that the argomanifest schema can be used from yamllsp
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.yaml", "*.yml" },
+  callback = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, 20, false)
+    for _, line in ipairs(lines) do
+      if line:match("^kind:%s*ApplicationSet") then
+        vim.bo.filetype = "argocd-applicationset"
+        return
+      elseif line:match("^kind:%s*Application") then
+        vim.bo.filetype = "argocd-application"
+        return
+      end
+    end
+  end,
+})
+-- register these new filetypes
+vim.filetype.add({
+  extension = {
+    ["argocd-applicationset"] = "yaml",
+    ["argocd-application"] = "yaml",
+  },
+})
+vim.treesitter.language.register("yaml", "argocd-applicationset")
+vim.treesitter.language.register("yaml", "argocd-application")
