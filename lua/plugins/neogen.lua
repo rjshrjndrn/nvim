5c7c11291a1c624fc960f7ad8e7c3274ccb47789
rjshrjndrn <rjshrjndrn@gmail.com>
2025-07-25 16:58:50 +0200

    feat(neogen): add keymap for generating docstrings
    Added <leader>cn keybinding to trigger Neogen's docstring generation.
    This improves workflow efficiency by allowing quick access to the
    Neogen generate function directly from normal mode.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,5 +1,15 @@
 return {
   "danymat/neogen",
+  cmd = "Neogen",
+  keys = {
+    {
+      "<leader>cn",
+      function()
+        require("neogen").generate()
+      end,
+      desc = "Generate docstring",
+    },
+  },
   opts = function(_, opts)
     -- https://github.com/danymat/neogen?tab=readme-ov-file#configuration
     opts.languages = {
