3604aa8a41f49b4bab11bc9e1a687cb97098cb22
rjshrjndrn <rjshrjndrn@gmail.com>
2025-07-25 17:18:59 +0200

    refactor(fugitive): remove amend commit keymap
    Removed the keymap for 'gca' which triggered 'Git commit --amend' to
    simplify the fugitive plugin key mappings and reduce potential for
    accidental amend operations.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -10,7 +10,6 @@ local opts = { silent = true }
 
 keymap("n", "gw", "<cmd>Gwrite<CR>", opts)
 keymap("n", "gc", "<cmd>Git commit --sign<CR>", opts)
-keymap("n", "gca", "<cmd>Git commit --amend<CR>", opts)
 keymap("n", "gs", "<cmd>Git<CR>", opts)
 
 return M
