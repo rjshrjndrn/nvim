53842e3b2054697617b5226686e2a1e87546faab
rjshrjndrn <rjshrjndrn@gmail.com>
2025-08-17 17:58:37 +0200

    fix(config): disable noice.nvim due to Gdiffsplit issue
    Temporarily disables folke/noice.nvim plugin as Gdiffsplit is not working
    with Neovim 0.12. This should help avoid related errors until a fix or
    update is available.
    
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,12 @@
 -- { "folke/flash.nvim", enabled = false },
 -- { "folke/todo-comments.nvim", enabled = true, commit = "ae0a2afb47cf7395dc400e5dc4e05274bf4fb9e0" },
+-- return {
+--   -- { commit = "7c36f6d459bd8a15c77fbfa75fbc843b2c18af87", "echasnovski/mini.ai" },
+-- }
 return {
-  -- { commit = "7c36f6d459bd8a15c77fbfa75fbc843b2c18af87", "echasnovski/mini.ai" },
+  -- Have issue with nvim 12 and Gdiffsplit is not working
+  {
+    "folke/noice.nvim",
+    enabled = false,
+  },
 }
