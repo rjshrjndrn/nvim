084fc773cbbe93c2a9963413971bf50500d83ca7
rjshrjndrn <rjshrjndrn@gmail.com>
2025-12-17 11:02:41 +0100

    refactor(comment): removed commit
    Signed-off-by: rjshrjndrn <rjshrjndrn@gmail.com>

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -179,17 +179,17 @@ return {
   { import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-      },
-    },
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "stylua",
+  --       "shellcheck",
+  --       "shfmt",
+  --       "flake8",
+  --     },
+  --   },
+  -- },
 
   -- Use <tab> for completion and snippets (supertab)
   {
