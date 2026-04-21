78123bd28c5ff5736054cfdfd278aeab336b68b1
rjshrjndrn <rjshrjndrn@gmail.com>
2026-04-21 14:22:50 +0200

    vimwiki: move config to init

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,29 +2,27 @@ local M = {
   {
     "vimwiki/vimwiki",
     cmd = "VimwikiIndex",
+    event = "BufEnter *.md",
     keys = {
       { "<leader>ww", "<cmd>VimwikiIndex<cr>", desc = "Open VimWiki index" },
-      { "<leader>wi", "<cmd>VimwikiDiaryIndex<cr>", desc = "Open VimWiki index" },
+      { "<leader>wi", "<cmd>VimwikiDiaryIndex<cr>", desc = "Open VimWiki diary index" },
       { "<leader>w<leader>w", "<cmd>VimwikiMakeDiaryNote<cr>", desc = "Open VimWiki diary entry for today." },
     },
+    init = function()
+      vim.g.vimwiki_markdown_link_ext = 1
+      vim.g.taskwiki_markup_syntax = "markdown"
+      vim.g.markdown_folding = 1
+
+      vim.g.vimwiki_list = {
+        { path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
+        { path = "~/vimwiki_personal/", syntax = "markdown", ext = ".md" },
+      }
+      vim.g.vimwiki_ext2syntax =
+        { [".md"] = "markdown", [".mkd"] = "markdown", [".wiki"] = "media", [".rajesh"] = "markdown" }
+      vim.g.vimwiki_folding = ""
+      vim.g.vimwiki_listsyms = " ○◐●✓"
+    end,
   },
 }
-
-vim.g.vimwiki_markdown_link_ext = 1
-vim.g.taskwiki_markup_syntax = "markdown"
-vim.g.markdown_folding = 1
-
-vim.g.vimwiki_list = {
-  { path = "~/vimwiki/", syntax = "markdown", ext = ".md" },
-  { path = "~/vimwiki_personal/", syntax = "markdown", ext = ".md" },
-}
-vim.g.vimwiki_ext2syntax =
-  { [".md"] = "markdown", [".mkd"] = "markdown", [".wiki"] = "media", [".rajesh"] = "markdown" }
--- vim.g.vimwiki_ext2syntax = { md= 'markdown',
---     mkd= 'markdown',
---     wiki = 'media'
---   }
-vim.g.vimwiki_folding = ""
-vim.g.vimwiki_listsyms = " ○◐●✓"
 
 return M
